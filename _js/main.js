c3280b951d4ec93fcf47b1a25f4b129e4b2a1db7
Lucas <lucaskreuch77@gmail.com>
2022-11-03 20:23:59 -0300

    Desafio#3 - Livraria - Cadastro e login

--- _js/main.js
+++ _js/main.js
@@ -1,4 +1,4 @@
-var lista_usuarios = [];
+var lista_usuarios = new ListaUsuarios();
 
 $(document).ready(() => {
 
@@ -10,42 +10,71 @@ $(document).ready(() => {
 
     // Modal
     $(".btn-login").click((e) => {
-        $("#modal-login").modal("toggle"); //abre modal login
+        $("#modal-login").modal("toggle"); // Abre modal login
     });
 
-    $('.btn-cadastro').click((e)=>{
+    $('.btn-cadastro').click((e) => {
         $('#modal-cadastro').modal("toggle");
     });
 
-    $('.bclose').click((e) =>{ //fecha todos os modais
+    $('.bclose').click((e) => { //fecha todos os modais
         $('.modal').modal('hide');
     })
 
+    // Cadastro
+    $("#btn-cadastrar").click((e) => {
+        if (getJsonItem(LISTA_USUARIOS)) {
+            lista_usuarios = getJsonItem(LISTA_USUARIOS);
+        }
+
+        let tamLista = lista_usuarios == null ? 0 : lista_usuarios.usuarios.length;
+        let idInsert = tamLista + 1;
+
+        let usuario = new Usuario(
+            idInsert,
+            $("#nome-cadastro-input").val(),
+            $("#email-cadastro-input").val(),
+            $("#senha-cadastro-input").val(),
+            new Array()
+        );
+
+        try {
+            lista_usuarios.usuarios.push(usuario);
+        } catch {
+            lista_usuarios.push(usuario);
+        }
+
+        setJsonItem(LISTA_USUARIOS, lista_usuarios);
+        $("#modal-cadastro").modal("hide");
+    });
+
     //Login
-    $('#btn-cadastrar').click((e) =>{
-         if(VerificarLogin()){
-            console.log("verificou");
-            if(getJsonItem(LISTA_USUARIOS)){ // se retornar algo
-                 lista_usuarios = getJsonItem(LISTA_USUARIOS);  //recebe todos os usuários do localStorage
-            }
+    $('#btn-logar').click((e) => {
+        console.log(getJsonItem(LISTA_USUARIOS).usuarios)
+        if (getJsonItem(LISTA_USUARIOS)) {
+            getJsonItem(LISTA_USUARIOS).usuarios.forEach(user => {
+                if ($("#email-login-input").val() === user.email &&
+                    $("#senha-login-input").val() === user.senha) {
 
-            let tamLista = lista_usuarios == null ? 0 : lista_usuarios.length;
-            let idInsert = tamLista + 1; 
+                    let lista_resenhas = user.resenhas;
 
-            let nome = $('#nome-cadastro-input').val();
-            let senha = $('#senha-cadastro-input').val();
-            let email = $('#email-cadastro-input').val() 
+                    let usuario_logado = new Usuario(
+                        user.id,
+                        user.nome,
+                        user.email,
+                        user.senha,
+                        lista_resenhas
+                    );
 
-            user = new Usuario(idInsert, nome, senha, email);
-
-            lista_usuarios.push(user);
-            setJsonItem(LISTA_USUARIOS, lista_usuarios);
-        };
-
+                    setJsonItem(USER_LOGADO, usuario_logado);
+                    $("#modal-login").modal("hide");
+                    // window.location.href = ""  
+                }
+            });
+        }
     });
 
 });
-
 
 
 //FUNCTIONS
@@ -53,30 +82,30 @@ var getPagina = (page, target) => {
     $.ajax({
         url: page,
         datatype: 'html',
-        success: (data) =>{
+        success: (data) => {
             $(target).html(data);
         }
     })
 };
 
-var VerificarLogin = () =>{
+var VerificarLogin = () => {
     let nome = $('#nome-cadastro-input').val();
     let email = $('#email-cadastro-input').val();
     let senha = $('#senha-cadastro-input').val();
 
-    if(email == "" || senha == "" || nome == ""){
+    if (email == "" || senha == "" || nome == "") {
         return false;
-    }else{
+    } else {
         return VerificaEmail(email);
     }
 };
 
-var VerificaEmail = (email) =>{
+var VerificaEmail = (email) => {
     let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
 
-    if(regexEmail.test(email)){
+    if (regexEmail.test(email)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
